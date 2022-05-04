d81cadb039f288db6baf649598da70701f2df70f
Virajj <viraj.jadhav.rebase@gmail.com>
2022-05-04 15:55:18 +0530

    sc-1305 swagger livecheck with json

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const app = (0, express_1.default)();
+const js_yaml_1 = __importDefault(require("js-yaml"));
+const fs_1 = __importDefault(require("fs"));
+const doc = js_yaml_1.default.load(fs_1.default.readFileSync('./src/swagger/api.yaml', 'utf8'));
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(doc));
+app.get('/livecheck', (req, res) => {
+    res.send("OK");
 });
 app.listen(port, () => {
     console.log(`Express is listening at http://localhost:${port}`);
